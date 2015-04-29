a965d541e8d241182fc7e8eda7061681ce98d04c
jaehee kim <jaiyaa@hanmail.net>
2015-04-29 11:57:55 +0900

    main.js  업데이트

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,29 @@
         $.get("./header.html", function (data) {
             $("#header").html(data);
             var windowSize = $(window).width();
+            $(window).on('resize',function(){
+                windowSize = $(window).width();
+                if( windowSize >= 768 ) {
+                    dropdownHover();
+                } else {
+                    $('ul.nav li.dropdown').off('mouseenter mouseleave');
+                }
+            })
+
+            function dropdownHover() {
             $('ul.nav li.dropdown').hover(function() {
-                $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+                    var offsetLeft = parseInt($(this).offset().left);
+                    var subMenu = $(this).find('.dropdown-menu');
+                    subMenu.css({'padding-left': offsetLeft});
+                    if(subMenu.closest('.dropdown').hasClass('last')) {
+                        subMenu.css({'padding-left': offsetLeft - 150});
+                    }
             }, function() {
-                $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
             });
+            }
+            dropdownHover();
 
             // 2depth Menu 초기 셋팅
             function adjustMenu() {
@@ -19,7 +37,7 @@
                     if (idx < 3) {
                         subMenu.css({'padding-left': offsetLeft});
                     } else {
-                        subMenu.css({'padding-left': offsetLeft - 290});
+                        subMenu.css({'padding-left': offsetLeft - 150});
                     }
 
                 })
